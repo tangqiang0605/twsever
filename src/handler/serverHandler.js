b82691f6c334e3b19d8036bc46106ba4c17e5a77
tangqiang <3256224359@qq.com>
2022-11-04 17:54:43 +0800

    路由守卫、嵌套路由。

--- src/handler/serverHandler.js
+++ src/handler/serverHandler.js
@@ -8,43 +8,100 @@ const routeError = require('./routeError');
 const { ROUTER_CONFIG } = require('../../workplace/configs/config');
 const store = require('../store/index');
 
+const typeOf = require('../utils/typeOf');
+const { type } = require('os');
+
 const routePath = (routeName) => path.join(__dirname, '../' + ROUTER_CONFIG.routespath + '/', routeName);
 const brPath = (routeName) => path.join(__dirname, '../' + ROUTER_CONFIG.brpath + '/', routeName);
 const arPath = (routeName) => path.join(__dirname, '../' + ROUTER_CONFIG.arpath + '/', routeName);
 
-const serverHandler = async (req, res) => {
+async function handleRoute(curRoute) {
+  if (ROUTER_CONFIG.logwhenrunning || false) console.log('route:', curRoute, 'is running');
+  let route = require(routePath(curRoute));
+  if (typeOf(route) === 'Object') {
+    Object.keys(route).forEach(
+      async function (val) {
+        await route[`${val}`](req, res)
+          .then(result => endHandler(result))
+          .catch(err => routeError(err, i === routes.length || store.ismapper))
+      })
+  } else {
+    await require(routePath(curRoute))(req, res)
+      .then(result => endHandler(result))
+      .catch(err => routeError(err, i === routes.length - 1 || store.ismapper));
+  }
+}
+
+
+const serverHandler = async function (req, res) {
   // 数据处理
   await resHandler(res);
   await reqHandler(req);
   store.req = req;
   store.res = res;
 
-  // 路由前置守卫
-  // let beforeroutes = ROUTER_CONFIG.beforeroute || [];
-  // for (let i = 0; i < beforeroutes.length; i++) {
-  //   await require(brPath(routes[i]))(req, res)
-  //     .then(result => endHandler(result))
-  //     .catch(err => routeError(err, i === routes.length - 1));
-  // }
+  // 全局路由前置守卫
+  let isStop = false;
+  let beforeroutes = ROUTER_CONFIG.beforeroute || [];
+  for (let i = 0; i < beforeroutes.length; i++) {
+    await require(brPath(routes[i]))(req, res)
+      .then(result => result)
+      .catch(err => isStop = true);
+    if (isStop) {
+      return;
+    }
+  }
+
 
   let routes = ROUTER_CONFIG.routes || [];
   store.ismapper = false;
-  // 这里只允许一个路由js包含一个方法,也就是export只能导出一个方法而不能有多个.
-  // 后期将支持一个路由js中写多个方法并以数组或对象的形式导出,允许导出时多层嵌套(需要一个递归函数来实现)
-  for (let i = 0; i < routes.length&&(!store.ismapper); i++) {
-    if (ROUTER_CONFIG.logwhenrunning || false) console.log('route:', routes[i], 'is running');
-    await require(routePath(routes[i]))(req, res)
-      .then(result => endHandler(result))
-      .catch(err => routeError(err, i === routes.length - 1||store.ismapper));
+  isStop = false;
+  for (let i = 0; i < routes.length && (!store.ismapper); i++) {
+
+    let curRoute = routes[i];
+    if (typeOf(curRoute) === 'Object') {
+      // 在配置文件中使用{beforeroute:,route:,afterroute:}格式配置路由
+      if (!curRoute.route) {
+        // 局部前置路由守卫
+        if (curRoute.beforeroute) {
+          await (brPath(curRoute.beforeroute))(req, res).then(result => result).catch(err => isStop = true);
+          if (isStop) return;
+        }
+        // 局部路由
+        if (typeOf(curRoute.route) === 'Array') {
+          curRoute.route.forEach(async function (val) {
+            // 执行路由模块
+            handleRoute(val);
+          })
+        } else if (typeOf(curRoute.route) === 'String') {
+          // 执行路由模块
+          handleRoute(curRoute.route);
+        } else {
+          console.error('路由配置错误：只支持Array类型和String类型的值，不接受'
+            + typeOf(curRoute.route) + '类型的值！');
+        }
+        // 局部后置路由
+        if (curRoute.afterroute) {
+          await arPath(curRoute.afterroute)(req, res);
+        }
+
+      } else {
+        console.error('路由配置错误：局部路由配置必须包含至少一个route');
+      }
+
+    } else if (typeOf(curRoute) === 'Function') {
+      // 执行路由模块
+      handleRoute(curRoute);
+    } else {
+      console.error('路由模块错误：导出的类型必须是函数或者对象')
+    }
   }
 
   // 路由后置守卫
-  // let afterroutes = ROUTER_CONFIG.afterroute || [];
-  // for (let i = 0; i < afterroutes.length; i++) {
-  //   await require(arPath(routes[i]))(req, res)
-  //     .then(result => endHandler(result))
-  //     .catch(err => routeError(err, i === routes.length - 1));
-  // }
+  let afterroutes = ROUTER_CONFIG.afterroute || [];
+  for (let i = 0; i < afterroutes.length; i++) {
+    await require(arPath(routes[i]))(req, res);
+  }
 }
 
 module.exports = serverHandler;
